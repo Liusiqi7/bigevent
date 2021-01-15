55cf31dc59988215173af3ab1656fd7353a58b8e
Lsq <siqiliu_178@163.com>
2021-01-15 18:48:31 +0800

    完成了首页index的布局

---
+++ assets/js/common.js
@@ -0,0 +1,15 @@
+// 统一配置url,headers,complete
+var baseurl = ' http://www.itcbc.com:8080';
+$.ajaxPrefilter(function (option) { 
+    option.url = baseurl + option.url;
+    option.headers = {
+        Authorization=localStorage.getItem('token')
+    }
+    option.complete = function(xhr){
+        var res = xhr.responseJSON;
+        if (res && res.status === 1 && res.message === '身份认证失败！') { 
+            localStorage.removeItem('token')
+            location.href = './login.html';
+        }
+    }
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -2,6 +2,22 @@
 $('.login a').on('click', function () {
     $('.login').hide().next().show();
 });
-$('.register a').on('click', function () { 
+$('.register a').on('click', function () {
     $('.login').show().next().hide();
+});
+// 表单提交--阻止默认行为--收集表单数据(查询字符串形式)--提交数据
+$('register form').on('submit', function (e) { 
+    e.preventDefault();
+    var data = $(this).serialize(); 
+    $.ajax({
+        url: '/api/reguser',
+        type: 'POST',
+        data: data,
+        success: function (res) { 
+            if (res.status === 0) { 
+                 
+            }
+        }
+
+    })
 })
